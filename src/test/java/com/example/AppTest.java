0abaebcbc014aa82eddc2a9cbb4746c252814847
Keerthi Vasan <keerthivasank83@gmail.com>
2026-04-16 08:45:25 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,10 +1,21 @@
-package com.example;
-import org.junit.Test;
-import static org.junit.Assert.*;
-public class AppTest {
-@Test
-public void testAdd() {
-App app = new App();
-assertEquals(5, app.add(2, 3));
-}
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class PaymentServiceTest {
+    PaymentService service = new PaymentService();
+
+    @Test
+    void testSuccessfulPayment() {
+        assertTrue(service.validatePayment(100.0, "1234567812345678"));
+    }
+
+    @Test
+    void testInvalidAmount() {
+        assertFalse(service.validatePayment(-10.0, "1234567812345678"));
+    }
+
+    @Test
+    void testInvalidCardFormat() {
+        assertFalse(service.validatePayment(50.0, "1234"));
+    }
 }
